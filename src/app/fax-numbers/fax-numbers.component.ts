28e33f468c57a3a396bfbb86cd296727a72241fc
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-12-27 16:03:15 -0500

    fax numbers component

--- src/app/fax-numbers/fax-numbers.component.ts
+++ src/app/fax-numbers/fax-numbers.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-fax-numbers',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatListModule, MatDividerModule, MatBadgeModule],
   templateUrl: './fax-numbers.component.html',
   styleUrl: './fax-numbers.component.css'
 })
