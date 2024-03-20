c91e1c1506537ae8d8040e92e3354b10a599cfa3
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2024-03-20 16:35:56 -0400

    email copy to clipboard on click

--- src/app/person-list/person-list.component.ts
+++ src/app/person-list/person-list.component.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
-  imports: [CommonModule, MatListModule, MatDividerModule, MatBadgeModule],
+  imports: [CommonModule, MatListModule, MatDividerModule, MatBadgeModule, MatIconModule, ClipboardModule],
   templateUrl: './person-list.component.html',
   styleUrl: './person-list.component.css'
 })
 export class PersonListComponent {
+  constructor(private snackBar: MatSnackBar) {};
+
   @Input() searchText: string = '';
   @Input() person: any;
   @Input() isMobile: any;
@@ -23,5 +28,11 @@ export class PersonListComponent {
 
     let phoneFormat = "(" + areaCode + ") " + middleDigits + "-" + endDigits;
     return phoneFormat;
+  }
+
+  copyEmail() {
+    this.snackBar.open('E-mail copied to clipbaord.', 'Close', {
+      duration: 4000,
+    });
   }
 }
